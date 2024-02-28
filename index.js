a432d96e2995240d997d594496c0d896417de218
Admin3 Triline <admin3@triline.kz>
2024-02-28 17:59:31 +0600

    Add functions for requet BeelineVPBX and Bitrix24, not testing!

--- index.js
+++ index.js
@@ -5,22 +5,95 @@ const Buffer = require("node:buffer")
 const compare_users = require("./compare_users.json");
 const config = require("./config.json");
 
-function getToken (login, password) {
-    axios.post('https://cloudpbx.beeline.kz/VPBX/Account/GetToken', {
-        'login': login,
-        'password': password
-    })
-    .then(res => {
-        console.log(res.data);
-        return res.data;
-    }).
-    catch(error => {
-        console.log(error);
-    });
+
+// TODO: Refactor this "date" and "today".
+const date = new Date();
+const today = {
+    str : `${date.getFullYear()}-${date.getMonth() > 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`,
+    strUTC : `${date.getFullYear()}-${date.getMonth() > 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`,
+    strYMD : `${date.getFullYear()}_${date.getMonth() > 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)}_${date.getDate()}`,
+    strDMY : `${date.getDate()}_${date.getMonth() > 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)}_${date.getFullYear()}`
 }
 
-function callRegister({user_phone_number, user_id, phone_number, call_start_date, type, crm_create = 1, crm_source, crm_entity_type, crm_entity_id, show = 0}) {
-     axios.post(`https://bitrix.triline.kz/rest/${config.user}/${config.token}/telephony.externalcall.register.json`,{
+main();
+
+function main() { // TODO: Сделать функцию сборщиком всех других функций.
+    console.log("Im work!");
+}
+
+async function getToken () {
+    try {
+        return await axios.post('https://cloudpbx.beeline.kz/VPBX/Account/GetToken', config.beeVPBX.auth)
+    } catch (error) {
+        console.warn(error.response.data);
+    }
+}
+
+async function callLog(token) {
+    try {
+        const response = await axios({
+            method: "post",
+            url: "http://cloudpbx.beeline.kz/VPBX/Stat/CallLog",
+            headers: {
+                'X-VPBX-API-AUTH-TOKEN' : `${config.beeVPBX.api_token}`,
+                Cookie: `BearerToken=${token}`
+            },
+            data: {
+                page: 1,
+                limit: 100,
+                orderBy: "StartDT",
+                ascending: false,
+                query: {
+                    // Direction: "inbound", // TODO: Проверить как работет без этого параметра. Нужно брать все звонки.
+                    ProfileID: config.beeVPBX.profileID,
+                    StartDT: {
+                        start: today.str,
+                        end: `${today.strYMD} 13:35:20.000`
+                    }
+                }
+            }
+        });
+        console.log(await response.data);
+        return await response.data;
+    } catch (error) {
+        console.warn(error);
+    }
+}
+
+async function getAudio(data, token) {
+    try {
+        const response = await axios({
+            method: "get",
+            url: "http://cloudpbx.beeline.kz/VPBX/Cloud/GetCallRecordContent",
+            headers: {
+                'X-VPBX-API-AUTH-TOKEN' : `${config.beeVPBX.api_token}`,
+                Cookie: `BearerToken=${token}`
+            },
+            params: {
+                sipCallId: data.CALL_ID,
+                asPreview: false
+            }
+        });
+        console.log(await response.data);
+        return await response.data;
+    } catch (error) {
+        console.warn(error.response.data);
+    }
+}
+
+function callRegister({
+    user_phone_number,
+    user_id,
+    phone_number,
+    call_start_date,
+    type,
+    crm_create = 1,
+    crm_source,
+    crm_entity_type,
+    crm_entity_id,
+    show = 0
+}) {
+     axios.post(`https://bitrix.triline.kz/rest/${config.bitrix.user}/${config.bitrix.token}/telephony.externalcall.register.json`,{
         "USER_PHONE_INNER": user_phone_number,
         "USER_ID": user_id,
         "PHONE_NUMBER": phone_number,
@@ -34,12 +107,13 @@ function callRegister({user_phone_number, user_id, phone_number, call_start_date
         console.log(res.data);
         return res.data;
     })
+    .catch(error => {
+        console.error(error);
+    })
 }
 
-
-
 function callFinish ({call_id, user_id, duration, cost = 0.00, cost_currency = 'KZT', failed_reaction, record_url, vote, add_to_chat = 0}) {
-    axios.post(`https://bitrix.triline.kz/rest/${config.user}/${config.token}/telephony.externalcall.finish.json`, {
+    axios.post(`https://bitrix.triline.kz/rest/${config.bitrix.user}/${config.bitrix.token}/telephony.externalcall.finish.json`, {
         "CALL_ID": call_id,
         "USER_ID": user_id,
         "DURATION": duration,
@@ -53,23 +127,21 @@ function callFinish ({call_id, user_id, duration, cost = 0.00, cost_currency = '
     })
     .then(res => {
         console.log(res.data);
+        fs.appendFile(`./tmp/log_${today.strDMY}.txt`, `\n${JSON.stringify(res.data)}`, err => {
+            if (err) throw err;
+            console.log('Log is done!');
+        });
+    })
+    .catch(error => {
+        console.error(error);
+    })
+    .finally(res => { // TODO: Необходимо настроить логирование
+
     });
 }
 
-function main() {
-    axios.get(`https://bitrix.triline.kz/rest/${config.user}/${config.token}/profile.json`)
-        .then(res => {
-            // console.log(res.data);
-            // const data = new Uint8Array(Buffer.from('Hello Node.js'));
-            // fs.writeFile("./tmp/logs.txt", data, 'utf8', (err) => {
-            //     console.error(err);
-            // })
-        });
+function setLog(res, func = '') {
+    fs.appendFile(`./tmp/log_${today.strDMY}.txt`, `\n${func}: ${JSON.stringify(res.data)}`, err => {
+        if (err) throw err;
+    });
 }
-
-main();
-
-fs.appendFileAsync("./tmp/logs.txt", "Hello world!", err => {
-    if (err) throw err;
-    console.log('Done');
-});
